dbc39afbcf7db660425cbcd2ac0aff6e53d46cd2
gauthamcity12 <gauthamcity12@gmail.com>
2015-05-30 17:35:59 -0400

    Added navbar responsive code

--- Scripts/jq.js
+++ Scripts/jq.js
@@ -21,3 +21,10 @@ $(function play(){
 
 $(document).ready(play());
 
+$(document).ready(function () {
+	$(".navbar-nav li a").click(function(event) {
+		$(".navbar-collapse").collapse('hide');
+	});
+});
+
+
